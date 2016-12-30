ae8c15f2bb3e51944ccebcd32e17e38527bf094d
Matt Wagner <m.wagner@dwdiesel.com>
2016-12-30 11:13:06 -0500

    Add method that retrieves the total asset count

--- PortfolioRestToken/Program.cs
+++ PortfolioRestToken/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -24,13 +25,20 @@ namespace PortfolioRestToken
         public string StorageType { get; set; }
     }
 
+    class Asset
+    {
+        public int TotalNumberOfAssets { get; set; }
+    }
+
     class Program
     {
         public static string TargetCatalogId { get; private set; }
+        public static int TotalNumberOfAssets { get; private set; }
 
         static void Main(string[] args)
         {
             GetCatalogs().Wait();
+            FindNumberOfAssetsAsync().Wait();
         }
 
         private static async Task GetCatalogs()
@@ -67,6 +75,45 @@ namespace PortfolioRestToken
                 catch (HttpRequestException e)
                 {
                     Console.WriteLine($"[ Get Catalogs ][ ERROR ! ] {e.Message}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task FindNumberOfAssetsAsync()
+        {
+            var findNumberUrl = $"{Constants.serverProtocol}://{Constants.serverAddress}:{Constants.serverPort}/api/v1/catalog/{TargetCatalogId}/asset/?session={Constants.apiToken}";
+            var findNumberData = "{\"fields\":[\"Item ID\",\"Filename\"],\"pageSize\":1,\"startingIndex\":0,\"sortOptions\":{\"field\":\"Cataloged\",\"order\":\"desc\"},}";
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var requestMessage = new HttpRequestMessage()
+                    {
+                        RequestUri = new Uri(findNumberUrl),
+                        Content = new StringContent(findNumberData, UnicodeEncoding.UTF8, "application/json")
+                    };
+                    var response = await client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
+                    response.EnsureSuccessStatusCode();
+                    var stringResponse = response.Content.ReadAsStringAsync().Result;
+                    var assets = JsonConvert.DeserializeObject<Asset>(stringResponse);
+
+                    if (assets.TotalNumberOfAssets == 0)
+                    {
+                        Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] No Assets Available!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[ Assets Output ] {stringResponse}");
+                        Console.WriteLine($"[ Total Number of Assets ] = {assets.TotalNumberOfAssets}");
+                        TotalNumberOfAssets = assets.TotalNumberOfAssets;
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] {e.Message}");
                 }
             }
 
