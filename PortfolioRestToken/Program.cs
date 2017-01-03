50acc302f2fc69e7a2845db4ddf9bc5ef7a9d8a1
Matt Wagner <m.wagner@dwdiesel.com>
2017-01-03 12:31:02 -0500

    Use a single instance of HttpClient in the program

--- PortfolioRestToken/Program.cs
+++ PortfolioRestToken/Program.cs
@@ -45,6 +45,9 @@ namespace PortfolioRestToken
 
     class Program
     {
+        // Use a single instance of HttpClient
+        private static HttpClient Client = new HttpClient();
+
         public static string TargetCatalogId { get; private set; }
         public static int TotalNumberOfAssets { get; private set; }
         public static int RandomId { get; private set; }
@@ -62,39 +65,36 @@ namespace PortfolioRestToken
 
         private static async Task GetCatalogsAsync()
         {
-            using (var client = new HttpClient())
+            try
             {
-                try
+                var catalogUrl = $"{Constants.serverProtocol}://{Constants.serverAddress}:{Constants.serverPort}/api/v1/catalog?session={Constants.apiToken}";
+                var response = await Client.GetAsync(catalogUrl);
+                response.EnsureSuccessStatusCode();
+
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                var catalogs = JsonConvert.DeserializeObject<IEnumerable<Catalog>>(stringResponse);
+
+                Console.WriteLine("[ Catalogs Output ]");
+                Console.WriteLine(stringResponse);
+                Console.WriteLine("The following Catalogs are available:");
+                foreach (var catalog in catalogs)
                 {
-                    var catalogUrl = $"{Constants.serverProtocol}://{Constants.serverAddress}:{Constants.serverPort}/api/v1/catalog?session={Constants.apiToken}";
-                    var response = await client.GetAsync(catalogUrl);
-                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine($"[ {catalog.Name} ]");
+                }
 
-                    var stringResponse = await response.Content.ReadAsStringAsync();
-
-                    var catalogs = JsonConvert.DeserializeObject<IEnumerable<Catalog>>(stringResponse);
-
-                    Console.WriteLine("[ Catalogs Output ]");
-                    Console.WriteLine(stringResponse);
-                    Console.WriteLine("The following Catalogs are available:");
-                    foreach (var catalog in catalogs)
+                foreach (var catalog in catalogs)
+                {
+                    if (catalog.Name == Constants.targetCatalog)
                     {
-                        Console.WriteLine($"[ {catalog.Name} ]");
-                    }
-
-                    foreach (var catalog in catalogs)
-                    {
-                        if (catalog.Name == Constants.targetCatalog)
-                        {
-                            Console.WriteLine($"[ {catalog.Name} ][ {catalog.Id} ]");
-                            TargetCatalogId = catalog.Id;
-                        }
+                        Console.WriteLine($"[ {catalog.Name} ][ {catalog.Id} ]");
+                        TargetCatalogId = catalog.Id;
                     }
                 }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine($"[ Get Catalogs ][ ERROR ! ] {e.Message}");
-                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"[ Get Catalogs ][ ERROR ! ] {e.Message}");
             }
         }
 
@@ -103,35 +103,32 @@ namespace PortfolioRestToken
             var findNumberUrl = $"{Constants.serverProtocol}://{Constants.serverAddress}:{Constants.serverPort}/api/v1/catalog/{TargetCatalogId}/asset/?session={Constants.apiToken}";
             var findNumberData = "{\"fields\":[\"Item ID\",\"Filename\"],\"pageSize\":1,\"startingIndex\":0,\"sortOptions\":{\"field\":\"Cataloged\",\"order\":\"desc\"},}";
 
-            using (var client = new HttpClient())
+            try
             {
-                try
+                var requestMessage = new HttpRequestMessage()
                 {
-                    var requestMessage = new HttpRequestMessage()
-                    {
-                        RequestUri = new Uri(findNumberUrl),
-                        Content = new StringContent(findNumberData, UnicodeEncoding.UTF8, "application/json")
-                    };
-                    var response = await client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
-                    response.EnsureSuccessStatusCode();
-                    var stringResponse = response.Content.ReadAsStringAsync().Result;
-                    var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
+                    RequestUri = new Uri(findNumberUrl),
+                    Content = new StringContent(findNumberData, UnicodeEncoding.UTF8, "application/json")
+                };
+                var response = await Client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
+                response.EnsureSuccessStatusCode();
+                var stringResponse = response.Content.ReadAsStringAsync().Result;
+                var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
 
-                    if (assets.TotalNumberOfAssets == 0)
-                    {
-                        Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] No Assets Available!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[ Assets Output ] {stringResponse}");
-                        Console.WriteLine($"[ Total Number of Assets ] = {assets.TotalNumberOfAssets}");
-                        TotalNumberOfAssets = assets.TotalNumberOfAssets;
-                    }
-                }
-                catch (HttpRequestException e)
+                if (assets.TotalNumberOfAssets == 0)
                 {
-                    Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] {e.Message}");
+                    Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] No Assets Available!");
                 }
+                else
+                {
+                    Console.WriteLine($"[ Assets Output ] {stringResponse}");
+                    Console.WriteLine($"[ Total Number of Assets ] = {assets.TotalNumberOfAssets}");
+                    TotalNumberOfAssets = assets.TotalNumberOfAssets;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"[ Find Number of Assets ][ ERROR ! ] {e.Message}");
             }
         }
 
@@ -141,35 +138,32 @@ namespace PortfolioRestToken
             int rnd = new Random().Next(0, TotalNumberOfAssets);
             var findIdData = $"{{\"fields\":[\"Item ID\",\"Filename\"],\"pageSize\":1,\"startingIndex\":{rnd},\"sortOptions\":{{\"field\":\"_id\",\"order\":\"desc\"}},}}";
 
-            using (var client = new HttpClient())
+            try
             {
-                try
+                var requestMessage = new HttpRequestMessage()
                 {
-                    var requestMessage = new HttpRequestMessage()
-                    {
-                        RequestUri = new Uri(findIdUrl),
-                        Content = new StringContent(findIdData, Encoding.UTF8, "application/json")
-                    };
-                    var response = await client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
-                    response.EnsureSuccessStatusCode();
-                    var stringResponse = response.Content.ReadAsStringAsync().Result;
-                    var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
+                    RequestUri = new Uri(findIdUrl),
+                    Content = new StringContent(findIdData, Encoding.UTF8, "application/json")
+                };
+                var response = await Client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
+                response.EnsureSuccessStatusCode();
+                var stringResponse = response.Content.ReadAsStringAsync().Result;
+                var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
 
-                    if (assets.TotalNumberOfAssets == 0)
-                    {
-                        Console.WriteLine($"[ Find Random ID ][ ERROR ! ] No Assets Available!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[ Assets Output ] {stringResponse}");
-                        Console.WriteLine($"[ Random Item ID ] = {assets.Assets[0].Id}");
-                        RandomId = assets.Assets[0].Id;
-                    }
-                }
-                catch (HttpRequestException e)
+                if (assets.TotalNumberOfAssets == 0)
                 {
-                    Console.WriteLine($"[ Find Random ID ][ ERROR ! ] {e.Message}");
+                    Console.WriteLine($"[ Find Random ID ][ ERROR ! ] No Assets Available!");
                 }
+                else
+                {
+                    Console.WriteLine($"[ Assets Output ] {stringResponse}");
+                    Console.WriteLine($"[ Random Item ID ] = {assets.Assets[0].Id}");
+                    RandomId = assets.Assets[0].Id;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"[ Find Random ID ][ ERROR ! ] {e.Message}");
             }
         }
 
@@ -178,39 +172,36 @@ namespace PortfolioRestToken
             var getAssetUrl = $"{Constants.serverProtocol}://{Constants.serverAddress}:{Constants.serverPort}/api/v1/catalog/{TargetCatalogId}/asset/?session={Constants.apiToken}";
             var getAssetData = $"{{\"fields\":[\"Item ID\",\"Filename\",\"Keywords\"],\"pageSize\":10,\"startingIndex\":0,\"sortOptions\":{{\"field\":\"_id\",\"order\":\"desc\"}},\"term\":{{\"operator\":\"equalValue\",\"field\":\"_id\",\"values\":[\"{RandomId}\"]}}}}";
 
-            using (var client = new HttpClient())
+            try
             {
-                try
+                var requestMessage = new HttpRequestMessage()
                 {
-                    var requestMessage = new HttpRequestMessage()
-                    {
-                        RequestUri = new Uri(getAssetUrl),
-                        Content = new StringContent(getAssetData, Encoding.UTF8, "application/json")
-                    };
-                    var response = await client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
-                    response.EnsureSuccessStatusCode();
-                    var stringResponse = response.Content.ReadAsStringAsync().Result;
-                    var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
+                    RequestUri = new Uri(getAssetUrl),
+                    Content = new StringContent(getAssetData, Encoding.UTF8, "application/json")
+                };
+                var response = await Client.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
+                response.EnsureSuccessStatusCode();
+                var stringResponse = response.Content.ReadAsStringAsync().Result;
+                var assets = JsonConvert.DeserializeObject<Response>(stringResponse);
 
-                    if (assets.TotalNumberOfAssets == 0)
-                    {
-                        Console.WriteLine($"[ Get Asset ][ ERROR ! ] No Assets Available!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[ Assets Output ] {stringResponse}");
-                        Console.WriteLine($"[ Random Item ID ] = {assets.Assets[0].Id}");
-
-                        RandomFilename = assets.Assets[0].Attributes.Filename[0];
-                        Console.WriteLine($"[ Random Filename ] = {RandomFilename}");
-
-                        SaveRandomPreviewAsync().Wait();
-                    }
-                }
-                catch (HttpRequestException e)
+                if (assets.TotalNumberOfAssets == 0)
                 {
-                    Console.WriteLine($"[ Get Asset ][ ERROR ! ] {e.Message}");
+                    Console.WriteLine($"[ Get Asset ][ ERROR ! ] No Assets Available!");
                 }
+                else
+                {
+                    Console.WriteLine($"[ Assets Output ] {stringResponse}");
+                    Console.WriteLine($"[ Random Item ID ] = {assets.Assets[0].Id}");
+
+                    RandomFilename = assets.Assets[0].Attributes.Filename[0];
+                    Console.WriteLine($"[ Random Filename ] = {RandomFilename}");
+
+                    SaveRandomPreviewAsync().Wait();
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"[ Get Asset ][ ERROR ! ] {e.Message}");
             }
         }
 
@@ -231,16 +222,13 @@ namespace PortfolioRestToken
 
             Console.WriteLine($"[ Save Random Item's Preview ][ Saving ! ][ {RandomFilename} ]");
 
-            using (var client = new HttpClient())
+            using (HttpResponseMessage response = await Client.GetAsync(saveRandomUrl, HttpCompletionOption.ResponseHeadersRead))
+            using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
             {
-                using (HttpResponseMessage response = await client.GetAsync(saveRandomUrl, HttpCompletionOption.ResponseHeadersRead))
-                using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
+                string fileToWriteTo = Path.Combine(Constants.imagesPath, RandomFilename);
+                using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
                 {
-                    string fileToWriteTo = Path.Combine(Constants.imagesPath, RandomFilename);
-                    using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
-                    {
-                        await streamToReadFrom.CopyToAsync(streamToWriteTo);
-                    }
+                    await streamToReadFrom.CopyToAsync(streamToWriteTo);
                 }
             }
         }
